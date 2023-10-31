8fd6d524f34c63ac024fe1fdd5d54e79f24d86d1
James Hallett <jamesbown42@gmail.com>
2023-10-31 17:19:19 +0000

    Initial Commit

---
+++ Nerv_mk1/Nerv_mk1App.swift
@@ -0,0 +1,17 @@
+//
+//  Nerv_mk1App.swift
+//  Nerv_mk1
+//
+//  Created by James Hallett on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nerv_mk1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
