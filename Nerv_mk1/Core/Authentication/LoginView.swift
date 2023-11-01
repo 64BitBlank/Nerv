31e8827d03f6382fad263ed50f921e0000b38987
James Hallett <jamesbown42@gmail.com>
2023-11-01 15:07:51 +0000

    setting up development branch

---
+++ Nerv_mk1/Core/Authentication/LoginView.swift
@@ -0,0 +1,28 @@
+//
+//  LoginView.swift
+//  Nerv_mk1
+//
+//  Created by James Hallett on 01/11/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                // image
+                
+                //form fields
+                
+                //sign in button
+                
+                //sign up button
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
